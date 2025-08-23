9e349415d01da942bc47ba346ebe4c5c6dcd1078
Samuel Mizato <samuel.mizato@gmail.com>
2025-08-23 00:12:16 -0300

    2. Leitura de Arquivos CSV

--- arquivos/2/CsvFileReader.java
+++ arquivos/2/CsvFileReader.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
+import java.io.IOException;     
 
 public class CsvFileReader {
     public static void main(String[] args) {
