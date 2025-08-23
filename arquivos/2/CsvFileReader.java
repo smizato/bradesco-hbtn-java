64b03d88fc23f8f30855eadbb97120f98baf2091
Samuel Mizato <samuel.mizato@gmail.com>
2025-08-22 23:55:35 -0300

    2. Leitura de Arquivos CSV

---
+++ arquivos/2/CsvFileReader.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CsvFileReader {
+    public static void main(String[] args) {
+        String fileName = "funcionarios.csv";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String linha;
+
+            while ((linha = reader.readLine()) != null) {
+                System.out.printf("Funcionário: %s\n", linha.split(",")[0]);
+                System.out.printf("Idade: %s\n", linha.split(",")[1]);
+                System.out.printf("Departamento: %s\n", linha.split(",")[2]);
+                System.out.printf("Salarial: %s\n", linha.split(",")[3]);
+                System.out.println("------------------------");
+            }
+            System.err.println("Leitura do arquivo concluída.");
+        } catch (IOException e) {
+            System.out.printf("Erro ao ler o arquivo: %s \n", e.getMessage());
+        }
+
+    }
+}
