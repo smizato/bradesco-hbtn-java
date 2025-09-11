5e97424863cac4c04c2ed76022ed5b9ee97137ab
Samuel Mizato <samuel.mizato@gmail.com>
2025-09-10 22:11:19 -0300

    0. Armazém de itens

---
+++ Generics_Exercises/armazem/Comida.java
@@ -0,0 +1,16 @@
+public class Comida {
+    private String nome;
+    private int calorias;
+    private double preco;
+
+    public Comida(String nome, int calorias, double preco) {
+        this.nome = nome;
+        this.calorias = calorias;
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %d R$ %.1f", nome, calorias, preco);
+    }
+}
