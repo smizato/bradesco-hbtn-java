c551672de172e3b26549a1ce3b23b2eb2df18616
Samuel Mizato <samuel.mizato@gmail.com>
2025-09-10 21:39:55 -0300

    Correction of "1. Complexidade O(n) – Busca Linear"

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -23,7 +23,7 @@ public class PessoasArray {
         System.out.println("Procurando pelo nome: \"" + nome + "\"");
         for (int i = 0; i < nomes.length; i++) {
             System.out.println("Passando pelo indice: " + i);
-            if (nomes[i].equalsIgnoreCase(nome)) {
+            if (nomes[i].equals(nome)) {
                 System.out.printf("Nome pesquisado é %s que está na posição %d\n", nome, i);
                 encontrado = true;
             }
