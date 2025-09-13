4def067c03ef091d9dc13372082829155602a3d3
Samuel Mizato <samuel.mizato@gmail.com>
2025-09-12 21:57:11 -0300

    1. Consultar Produtos com Predicado

---
+++ Lambda_Exercicios/predicado/Produto.java
@@ -0,0 +1,40 @@
+public class Produto {
+    private String nome;
+    private double preco;
+    private double peso;
+    private int quantidadeEmEstoque;
+    private TiposProduto tipo;
+
+    public Produto(String nome, double preco, double peso, int quantidadeEmEstoque, TiposProduto tipo) {
+        this.nome = nome;
+        this.preco = preco;
+        this.peso = peso;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+        this.tipo = tipo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public TiposProduto getTipo() {
+        return tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " " + preco + " " + peso + " " + quantidadeEmEstoque + " " + tipo;
+    }
+}
